575320a31898ec54495c4fd360f21c36f6cf0fea
German Eichemberger <germaneichemberger@gmail.com>
2023-11-11 15:15:03 -0300

    adds forecast command

---
+++ src/commands/forecast.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+  .setName('forecast')
+  .setDescription('Replies with the weather forecast for the given location')
+  .addStringOption((option) => {
+    option.setName('location')
+      .setDescription('The location to check the weather for')
+      .setRequired(true);
+
+    return option;
+  }).addStringOption((option) => {
+    option.setName('unit')
+      .setDescription('The unit to use for the temperature: eithcer Celsius or Fahrenheit')
+      .setRequired(false)
+      .addChoices(
+          {
+          name: 'Metric',
+          value: 'metric',
+        },
+        {
+          name: 'Imperial',
+          value: 'imperial',
+        });
+
+    return option;
+  });
+
+async function execute(interaction) {
+  const location = interaction.options.getString('location');
+  const unit = interaction.options.getString('unit') || 'metric';
+
+  await interaction.reply('The weather is great!');
+}
+
+module.exports = {
+  data,
+  execute,
+};
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const {
 const { clientReadyHandler } = require('./events/clientReady');
 
 const pingCommand = require('./commands/ping');
+const forecastCommand = require('./commands/forecast');
 const { interactionCreateHandler } = require('./events/interactionsCreate');
 
 const client = new Client({
@@ -19,6 +20,7 @@ const client = new Client({
 
 client.commands = new Collection();
 client.commands.set(pingCommand.data.name, pingCommand);
+client.commands.set(forecastCommand.data.name, forecastCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
 client.on(Events.InteractionCreate, interactionCreateHandler);
